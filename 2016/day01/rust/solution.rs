b33f8d2683c220175febfd6677c224ddb9cd270b
Nick Allen <rutherford.t.b@gmail.com>
2023-08-22 18:23:51 -0500

    add rust, verify rust testing strategy

---
+++ 2016/day01/rust/solution.rs
@@ -0,0 +1,14 @@
+fn part_one() -> Option<usize> {
+    const result: usize = 32;
+    return None;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn returns_value() {
+        assert!(part_one() != None);
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+pub mod day01_2016 {
+    include!("../2016/day01/rust/solution.rs");
+}
+
+pub mod day02_2016 {
+    include!("../2016/day02/rust/solution.rs");
+}
