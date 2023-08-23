b0bcfcd72b8d68dfa35197638400fe2a34221d22
Nick Allen <rutherford.t.b@gmail.com>
2023-08-23 15:02:22 -0500

    solve ts_01_2016

--- 2016/day01/typescript/solution.test.ts
+++ 2016/day01/typescript/solution.test.ts
@@ -5,14 +5,33 @@ import { expect, test } from "vitest";
 const input = readFileSync(`${__dirname}/../input`).toString();
 
 test("has partOne answer", () => {
-  expect(partOne(input)).toBeDefined();
+    expect(partOne(input)).toBeDefined();
 });
 
-test("has partTwo answer", () => {
-  expect(partOne(input)).toBeDefined();
+const partOneCases = [
+    ["R2, L3", 5],
+    ["R2, R2, R2", 2],
+    ["R5, L5, R5, R3", 12],
+] as const;
+
+partOneCases.forEach((v, i) => {
+    test("partOne: case " + i, () => {
+        expect(partOne(v[0])).toEqual(v[1]);
+    });
+});
+
+const partTwoCases = [["R8, R4, R4, R8", 4]] as const;
+
+partTwoCases.forEach((v, i) => {
+    test("partTwo: case " + i, () => {
+        expect(partTwo(v[0])).toEqual(v[1]);
+    });
 });
 
 test("has input", () => {
-  expect(input.length).toBeGreaterThan(0);
-  expect(input).toBeDefined();
+    expect(input.length).toBeGreaterThan(0);
+    expect(input).toBeDefined();
 });
+
+console.log("partOne answer", partOne(input));
+console.log("partTwo answer", partTwo(input));
--- 2016/day01/typescript/solution.ts
+++ 2016/day01/typescript/solution.ts
@@ -1,7 +1,87 @@
+const directions = ["N", "E", "S", "W"] as const;
+
 export function partOne(input: string) {
-  return;
+    const steps = parseInput(input);
+    let x = 0;
+    let y = 0;
+
+    let direction: (typeof directions)[number] = directions[0];
+
+    for (let step of steps) {
+        let turn = step[0];
+        let distance = parseInt(step.substring(1));
+
+        if (turn === "R") {
+            direction = directions[directions.indexOf(direction) + 1] || "N";
+        } else {
+            direction = directions[directions.indexOf(direction) - 1] || "W";
+        }
+
+        switch (direction) {
+            case "N":
+                y += distance;
+                break;
+            case "E":
+                x += distance;
+                break;
+            case "S":
+                y -= distance;
+                break;
+            case "W":
+                x -= distance;
+                break;
+        }
+    }
+
+    return Math.abs(x) + Math.abs(y);
 }
 
 export function partTwo(input: string) {
-  return;
+    const steps = parseInput(input);
+    let x = 0;
+    let y = 0;
+
+    let map = new Map<string, boolean>();
+
+    let direction: (typeof directions)[number] = directions[0];
+
+    for (let step of steps) {
+        let turn = step[0];
+        let distance = parseInt(step.substring(1));
+
+        if (turn === "R") {
+            direction = directions[directions.indexOf(direction) + 1] || "N";
+        } else {
+            direction = directions[directions.indexOf(direction) - 1] || "W";
+        }
+
+        for (let i = 0; i < distance; i++) {
+            switch (direction) {
+                case "N":
+                    y++;
+                    break;
+                case "E":
+                    x++;
+                    break;
+                case "S":
+                    y--;
+                    break;
+                case "W":
+                    x--;
+                    break;
+            }
+
+            if (map.get(`${x},${y}`)) {
+                return Math.abs(x) + Math.abs(y);
+            }
+
+            map.set(`${x},${y}`, true);
+        }
+    }
+
+    throw new Error("never crossed paths");
+}
+
+function parseInput(input: string) {
+    return input.split(/,\s/g);
 }
