e5ba694b210893e3e802cd275908f3f1d52eff26
Nick Allen <rutherford.t.b@gmail.com>
2023-08-24 14:24:53 -0500

    solve python01_2016_2

--- 2016/day01/python/solution.py
+++ 2016/day01/python/solution.py
@@ -1,5 +1,7 @@
+directions = ["N", "E", "S", "W"]
+
+
 def part_one(input):
-    directions = ["N", "E", "S", "W"]
     facing = "N"
     position = {'x': 0, 'y': 0}
 
@@ -21,6 +23,34 @@ def part_one(input):
     return abs(position['x']) + abs(position['y'])
 
 
+def part_two(input):
+    facing = "N"
+    position = {'x': 0, 'y': 0}
+    visited = set()
+
+    for step in input.strip().split(", "):
+        turn = step[0]
+        count = int(step[1:])
+
+        if turn == "L":
+            facing = directions[(directions.index(facing) - 1) %
+                                4]
+        elif turn == "R":
+            facing = directions[(directions.index(facing) + 1) %
+                                4]
+        else:
+            raise Exception("Invalid Turn")
+
+        for _ in range(0, count):
+            move(position, facing, 1)
+            loc = f"{position['x']},{position['y']}"
+
+            if loc in visited:
+                return abs(position['x']) + abs(position['y'])
+            else:
+                visited.add(loc)
+
+
 def move(position, direction, distance):
     if direction == "N":
         position['y'] += distance
@@ -33,4 +63,6 @@ def move(position, direction, distance):
 
 
 with open("../input", "r") as file:
-    print(part_one(file.read()))
+    input = file.read()
+    print("part one:", part_one(input))
+    print("part two:", part_two(input))
--- 2016/day01/python/test_solution.py
+++ 2016/day01/python/test_solution.py
@@ -1,7 +1,11 @@
-from solution import part_one
+from solution import part_one, part_two
 
 
 def test_part_one():
     assert part_one("R2, L3") == 5
     assert part_one("R2, R2, R2") == 2
     assert part_one("R5, L5, R5, R3") == 12
+
+
+def test_part_two():
+    assert part_two("R8, R4, R4, R8") == 4
