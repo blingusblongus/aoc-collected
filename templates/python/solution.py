e9eab42ed06ee8dbbe60ed2be7f7ba7cb445bd48
Nick Allen <rutherford.t.b@gmail.com>
2023-08-25 10:48:10 -0500

    add and improve python test template

---
+++ templates/python/solution.py
@@ -0,0 +1,12 @@
+def part_one(input):
+    return None
+
+
+def part_two(input):
+    return None
+
+
+with open("../input", "r") as file:
+    input = file.read()
+    print("part one:", part_one(input))
+    print("part two:", part_two(input))
---
+++ templates/python/test_solution.py
@@ -0,0 +1,27 @@
+from solution import part_one, part_two
+
+part_one_cases = []
+part_one_answer = None
+part_two_cases = []
+part_two_answer = None
+
+
+def test_part_one():
+    for case in part_one_cases:
+        assert part_one(case[0]) == case[1]
+
+
+def test_part_two():
+    for case in part_one_cases:
+        assert part_one(case[0]) == case[1]
+
+
+# test against final answers (if known)
+with open("../input", "r") as file:
+    input = file.read()
+
+    if part_one_answer is not None:
+        assert part_one(input) == part_one_answer
+
+    if part_two_answer is not None:
+        assert part_one(input) == part_one_answer
