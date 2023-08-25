ccf403599c234ee9e5f768b18fdbd4874ed9e807
Nick Allen <rutherford.t.b@gmail.com>
2023-08-25 15:04:25 -0500

    add solution ts_2022_02_2

--- 2022/day02/typescript/solution.ts
+++ 2022/day02/typescript/solution.ts
@@ -39,5 +39,36 @@ export function partOne(input: string) {
 }
 
 export function partTwo(input: string) {
-    return null;
+    let matches = input
+        .trim()
+        .split(/\n+/g)
+        .map((match) => match.split(/\s/g));
+
+    let choiceThem = ["A", "B", "C"];
+
+    let scores: number[] = [];
+    for (let match of matches) {
+        let score = 0;
+        const [them, desiredResult] = match;
+
+        const iThem = choiceThem.indexOf(them);
+
+        switch (desiredResult) {
+            case "X":
+                //lose
+                score = iThem == 0 ? 3 : iThem;
+                break;
+            case "Y":
+                //draw
+                score = iThem + 1 + 3;
+                break;
+            case "Z":
+                score = iThem == 2 ? 7 : iThem + 8;
+                break;
+        }
+
+        scores.push(score);
+    }
+
+    return scores.reduce((sum, score) => (sum += score), 0);
 }
