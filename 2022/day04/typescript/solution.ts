885ae29b29039e5174aa68d1dba7f70ea39dbd61
Nick Allen <rutherford.t.b@gmail.com>
2023-08-25 15:43:05 -0500

    refactor ts_2022_04 for better performance

--- 2022/day04/typescript/solution.ts
+++ 2022/day04/typescript/solution.ts
@@ -1,21 +1,7 @@
-function parseInput(input: string): number[][][] {
-    return input
-        .trim()
-        .split(/\n/g)
-        .map((el) =>
-            el.split(",").map((el) => el.split("-").map((el) => Number(el)))
-        );
-}
-
 export function partOne(input: string) {
     let count = 0;
     for (let pair of parseInput(input)) {
         const [first, second] = pair;
-
-        const checkContain = (x: number[], y: number[]): boolean => {
-            if (x[0] <= y[0] && x[1] >= y[1]) return true;
-            return false;
-        };
 
         if (checkContain(first, second) || checkContain(second, first)) count++;
     }
@@ -28,13 +14,27 @@ export function partTwo(input: string) {
     for (let pair of parseInput(input)) {
         const [first, second] = pair;
 
-        const checkOverlap = (x: number[], y: number[]): boolean => {
-            if (x[1] >= y[0] && y[1] >= x[0]) return true;
-            return false;
-        };
-
         if (checkOverlap(first, second)) count++;
     }
 
     return count;
 }
+
+function parseInput(input: string): number[][][] {
+    return input
+        .trim()
+        .split(/\n/g)
+        .map((el) =>
+            el.split(",").map((el) => el.split("-").map((el) => Number(el)))
+        );
+}
+
+const checkContain = (x: number[], y: number[]): boolean => {
+    if (x[0] <= y[0] && x[1] >= y[1]) return true;
+    return false;
+};
+
+const checkOverlap = (x: number[], y: number[]): boolean => {
+    if (x[1] >= y[0] && y[1] >= x[0]) return true;
+    return false;
+};
