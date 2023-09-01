9f09b9b3e873d7e2af9920772b223c59564da097
Nick Allen <rutherford.t.b@gmail.com>
2023-09-01 10:45:39 -0500

    init solution ts_11_2021_1

---
+++ 2021/day11/typescript/solution.test.ts
@@ -0,0 +1,165 @@
+import { Cavern, partOne, partTwo } from "./solution";
+import { readFileSync } from "fs";
+import { expect, test, describe, assert } from "vitest";
+
+const input = readFileSync(`${__dirname}/../input`).toString();
+
+const useFullInput = true;
+const partOneCases = [] as const;
+const partOneAnswer = 1688;
+const partTwoCases = [] as const;
+const partTwoAnswer = null;
+
+describe("solution folder", () => {
+    test("has input", () => {
+        expect(input.length).toBeGreaterThan(0);
+        expect(input).toBeDefined();
+    });
+});
+
+describe("cavern class", () => {
+    const cavern = new Cavern(`11111
+19991
+19191
+19991
+11111`);
+    test("gets all neighbors", () => {
+        expect(cavern.getNeighbors({ x: 1, y: 1 })).toEqual([
+            { x: 0, y: 0 },
+            { x: 0, y: 1 },
+            { x: 0, y: 2 },
+            { x: 1, y: 0 },
+            { x: 1, y: 2 },
+            { x: 2, y: 0 },
+            { x: 2, y: 1 },
+            { x: 2, y: 2 },
+        ]);
+    });
+
+    test("gets partial neighbors on edge", () => {
+        expect(cavern.getNeighbors({ x: 4, y: 0 })).toEqual([
+            { x: 3, y: 0 },
+            { x: 3, y: 1 },
+            { x: 4, y: 1 },
+        ]);
+    });
+
+    describe("matches first example", () => {
+        const cavern = new Cavern(`11111
+19991
+19191
+19991
+11111`);
+        test("simulates one step:", () => {
+            expect(cavern.simulate(1).toString()).toEqual(`34543
+40004
+50005
+40004
+34543`);
+        });
+
+        test("simulates two steps:", () => {
+            expect(cavern.simulate(1).toString()).toEqual(`45654
+51115
+61116
+51115
+45654`);
+        });
+    });
+
+    describe("matches second (larger) example", () => {
+        const largeCavern = `5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526`;
+        test("simulates 1 steps", () => {
+            const cavern = new Cavern(largeCavern);
+            expect(cavern.simulate(1).toString()).toEqual(`6594254334
+3856965822
+6375667284
+7252447257
+7468496589
+5278635756
+3287952832
+7993992245
+5957959665
+6394862637`);
+        });
+        test("simulates 2 steps", () => {
+            const cavern = new Cavern(largeCavern);
+            expect(cavern.simulate(2).toString()).toEqual(`8807476555
+5089087054
+8597889608
+8485769600
+8700908800
+6600088989
+6800005943
+0000007456
+9000000876
+8700006848`);
+        });
+        test("counts flashes after 10 steps", () => {
+            const cavern = new Cavern(largeCavern);
+            expect(cavern.simulate(10).flashes).toEqual(204);
+        });
+        test("counts flashes after 100 steps", () => {
+            const cavern = new Cavern(largeCavern);
+            expect(cavern.simulate(100).flashes).toEqual(1656);
+        });
+        // test("finds syncronization step", () => {
+        //     const cavern = new Cavern(largeCavern);
+        //     expect(cavern.syncsAt()).toEqual(195);
+        // });
+    });
+});
+
+describe("partOne", () => {
+    partOneCases.forEach((v, i) => {
+        test("fulfills case " + i, () => {
+            let result = partOne(v[0]);
+            expect(result).toBeDefined();
+            expect(result).is.not.null;
+            expect(result).toEqual(v[1]);
+        });
+    });
+
+    if (partOneAnswer && useFullInput) {
+        test("matches partOne answer (if known)", () => {
+            let result = partOne(input);
+            expect(result).toBeDefined();
+            expect(result).is.not.null;
+            expect(result).toEqual(partOneAnswer);
+        });
+    }
+});
+
+describe("partTwo", () => {
+    partTwoCases.forEach((v, i) => {
+        test("fulfills case " + i, () => {
+            let result = partTwo(v[0]);
+            expect(result).toBeDefined();
+            expect(result).is.not.null;
+            expect(result).toEqual(v[1]);
+        });
+    });
+
+    if (partTwoAnswer && useFullInput) {
+        test("matches partTwo answer (if known)", () => {
+            let result = partTwo(input);
+            expect(result).toBeDefined();
+            expect(result).is.not.null;
+            expect(partTwo(input)).toEqual(partTwoAnswer);
+        });
+    }
+});
+
+if (useFullInput) {
+    console.log("partOne answer", partOne(input));
+    console.log("partTwo answer", partTwo(input));
+}
---
+++ 2021/day11/typescript/solution.ts
@@ -0,0 +1,116 @@
+type Point = { x: number; y: number };
+export class Cavern {
+    private _grid: number[][];
+    public readonly width: number;
+    public readonly height: number;
+    private _flashes: number = 0;
+
+    constructor(input: string) {
+        this._grid = this.parseInput(input);
+        this.height = this._grid.length;
+        this.width = this._grid[0].length;
+    }
+
+    get flashes() {
+        return this._flashes;
+    }
+
+    public simulate(steps: number) {
+        for (let i = 0; i < steps; ++i) {
+            this.step();
+        }
+        return this;
+    }
+
+    public toString() {
+        return this._grid.map((row) => row.join("")).join("\n");
+    }
+
+    public getNeighbors({ x, y }: Point) {
+        const result: Point[] = [];
+
+        for (let i = -1; i <= 1; ++i) {
+            const newX = x + i;
+            if (newX < 0 || newX >= this.width) continue;
+            for (let j = -1; j <= 1; ++j) {
+                const newY = y + j;
+                if (newX === x && newY === y) continue;
+                if (newY < 0 || newY >= this.height) continue;
+                result.push({ x: newX, y: newY });
+            }
+        }
+
+        return result;
+    }
+
+    private parseInput(input: string) {
+        return input
+            .trim()
+            .split(/\s*\n/g)
+            .map((row) =>
+                row
+                    .trim()
+                    .split("")
+                    .map((octopus) => parseInt(octopus))
+            );
+    }
+
+    public getOctopusValue({ x, y }: Point) {
+        return this._grid[y][x];
+    }
+
+    private step() {
+        for (let y = 0; y < this.height; ++y) {
+            for (let x = 0; x < this.width; ++x) {
+                this._grid[y][x] += 1;
+            }
+        }
+
+        const flashes: Point[] = [];
+        for (let y = 0; y < this.height; ++y) {
+            for (let x = 0; x < this.width; ++x) {
+                let val = this.getOctopusValue({ x, y });
+                if (val > 9) {
+                    flashes.push({ x, y });
+                }
+            }
+        }
+
+        for (let point of flashes) {
+            this.flash(point);
+        }
+        for (let y = 0; y < this.height; ++y) {
+            for (let x = 0; x < this.width; ++x) {
+                let val = this.getOctopusValue({ x, y });
+                if (val > 9) {
+                    this._grid[y][x] = 0;
+                }
+            }
+        }
+
+        return this;
+    }
+
+    private flash(point: Point) {
+        this._flashes += 1;
+
+        let neighbors = this.getNeighbors(point);
+
+        for (let neighbor of neighbors) {
+            if (this.getOctopusValue(neighbor) > 9) continue;
+            this._grid[neighbor.y][neighbor.x] += 1;
+            if (this.getOctopusValue(neighbor) > 9) {
+                this.flash(neighbor);
+            }
+        }
+    }
+}
+
+export function partOne(input: string) {
+    const cavern = new Cavern(input);
+    return cavern.simulate(100).flashes;
+}
+
+export function partTwo(input: string) {
+    return null;
+}
