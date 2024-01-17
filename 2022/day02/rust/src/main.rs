2379926a698b963bb90a8c5e18479d8b35061d51
Nick Allen <rutherford.t.b@gmail.com>
2024-01-17 15:24:36 -0600

    solve rs_2022_2_2

--- 2022/day02/rust/src/main.rs
+++ 2022/day02/rust/src/main.rs
@@ -1,9 +1,10 @@
 use std::fs;
 
+#[derive(Debug, Clone, Copy)]
 enum Outcome {
-    Win,
-    Lose,
-    Draw,
+    Win = 6,
+    Lose = 0,
+    Draw = 3,
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -15,9 +16,9 @@ enum Hand {
 
 #[derive(Debug)]
 enum OppHand { 
-    A,
-    B,
-    C,
+    A = 1,
+    B = 2,
+    C = 3,
 }
 
 impl Hand {
@@ -33,6 +34,14 @@ impl Hand {
             _ => Err("Invalid Hand")
         }
     }
+
+    fn desired_outcome(self) -> Outcome {
+        match self {
+            Hand::X => Outcome::Lose,
+            Hand::Y => Outcome::Draw,
+            Hand::Z => Outcome::Win,
+        }
+    }
 }
 
 impl OppHand {
@@ -43,6 +52,32 @@ impl OppHand {
             "C" => Ok(OppHand::C),
             _ => Err("Invalid Hand")
         }
+    }
+
+    fn choose_response(self, &outcome: &Outcome) -> Hand {
+        match outcome {
+            Outcome::Win => match self {
+                OppHand::A => Hand::Y,
+                OppHand::B => Hand::Z,
+                OppHand::C => Hand::X,
+            },
+            Outcome::Draw => match self {
+                OppHand::A => Hand::X,
+                OppHand::B => Hand::Y,
+                OppHand::C => Hand::Z,
+            },
+            Outcome::Lose => match self {
+                OppHand::A => Hand::Z,
+                OppHand::B => Hand::X,
+                OppHand::C => Hand::Y,
+            },
+        }
+    }
+}
+
+impl Outcome {
+    fn point_value(self) -> u32 {
+        self as u32
     }
 }
 
@@ -68,17 +103,36 @@ fn part_one(input: &str) -> u32 {
             );
 
         player_score += score(opponent_hand, your_hand);
-        // println!("Current score = {}", player_score)
     }
     player_score
 }
 
-fn part_two(_input: &str) -> Option<u32> {
-    None
+fn part_two(input: &str) -> u32 {
+    let mut player_score = 0;
+    for line in input.lines() {
+        let hands: Vec<&str> = line.trim().split_whitespace().collect();
+
+        if hands.len() != 2 {
+            panic!("Improper number of hands.");
+        }
+
+        let (opponent_hand, your_hand) = (
+            OppHand::from_str(hands[0]).expect("Bad OppHand"), 
+            Hand::from_str(hands[1]).expect("Bad Hand")
+            );
+
+        let desired_outcome = your_hand.desired_outcome();
+        let your_choice = opponent_hand.choose_response(&desired_outcome);
+        println!("desired outcome: {:?}, your choice: {:?}", desired_outcome, your_choice);
+        // println!("Current score = {}", player_score)
+        player_score += your_choice.value();
+        player_score += desired_outcome.point_value();
+    }
+    player_score
 }
 
 fn score(opp_hand: OppHand, your_hand: Hand) -> u32 {
-    println!("{:?} vs {:?}", opp_hand, your_hand);
+    // println!("{:?} vs {:?}", opp_hand, your_hand);
 
     let outcome = match (opp_hand, your_hand) {
         (OppHand::A, Hand::Y) => Outcome::Win,
@@ -104,7 +158,7 @@ fn score(opp_hand: OppHand, your_hand: Hand) -> u32 {
         Outcome::Draw => 3,
         Outcome::Lose => 0,
     };
-    println!("{hand_value}, {win_value}");
+    // println!("{hand_value}, {win_value}");
     
     hand_value + win_value
 }
@@ -138,16 +192,14 @@ C Z"#;
     }
 
     #[test]
-    #[ignore]
     fn test_part_two_example() {
-        assert_eq!(part_two(&TEST_INPUT), Some(45000));
+        assert_eq!(part_two(&TEST_INPUT), 12);
     }
 
     #[test]
-    #[ignore]
     fn test_part_two_answer() {
         let input = fs::read_to_string("../input").expect("should read from file");
 
-        assert_eq!(part_two(&input), Some(213958));
+        assert_eq!(part_two(&input), 10498);
     }
 }
